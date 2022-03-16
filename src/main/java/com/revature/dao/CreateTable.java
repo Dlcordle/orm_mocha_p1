802675b108ebdf1ac9356ab092671b7403c016dd
Mark Deane <mddeane@yahoo.com>
2022-03-15 20:48:28 -0500

    Built CreateTables and fixed a typo in Table interface

--- src/main/java/com/revature/dao/CreateTable.java
+++ src/main/java/com/revature/dao/CreateTable.java
@@ -1,5 +1,154 @@
 package com.revature.dao;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+
 public class CreateTable {
 
+	private String tableName; // @Table tableName
+	private String tableSchema; // @Table tableSchema
+	private String primaryKeyColumnName; // @Id columnName
+
+	/**
+	 * @GeneratedValue(strategy=GenerationType.IDENTITY)
+	 * We need to save this somewhere when making meta models.
+	 */
+	private boolean primaryKeyIsSerial;
+	
+	/**
+	 * @Column
+	 * List of column data saved in a key=value LinkedHashMap
+	 * columnName=String
+	 * columnType=String of type name, use .getType() to get "String", "int", "double", "boolean"
+	 * isUnique=false
+	 * isNullable=true, *false* means NOT NULL
+	 * columnLength=50, if 0 defaults to 50
+	 * columnPrecision=2 if NUMERIC, else 0
+	 */
+	private List<LinkedHashMap<String, String>> columns; 
+	
+	/**
+	 * @JoinColumn
+	 * List of column data saved in a key=value LinkedHashMap
+	 * columnName=String
+	 * columnType=String of type name, use .getType() to get "String", "int", "double", "boolean"
+	 * isUnique=false
+	 * isNullable=true, *false* means NOT NULL
+	 * columnLength=50, if 0 defaults to 50
+	 * columnPrecision=2 if NUMERIC, else 0
+	 */
+	private List<LinkedHashMap<String, String>> foreignKeyColumns; 
+
+	// SQL statement to CREATE table
+	// We will build this throughout the program.
+	private String defineTableSQL = "";
+
+	// No args constructor
+	public CreateTable() {
+		super();
+	}
+
+	/**
+	 * CreateTable Constructor with all parameters
+	 * @param tableName
+	 * @param tableSchema
+	 * @param primaryKeyColumnName
+	 * @param primaryKeyIsSerial
+	 * @param columns
+	 * @param foreignKeyColumns
+	 */
+	public CreateTable(String tableName, String tableSchema, String primaryKeyColumnName, boolean primaryKeyIsSerial,
+			List<LinkedHashMap<String, String>> columns, List<LinkedHashMap<String, String>> foreignKeyColumns) {
+		super();
+		this.tableName = tableName;
+		this.tableSchema = tableSchema;
+		this.primaryKeyColumnName = primaryKeyColumnName;
+		this.primaryKeyIsSerial = primaryKeyIsSerial;
+		this.columns = columns;
+		this.foreignKeyColumns = foreignKeyColumns;
+
+	}
+
+	/**
+	 * This method does all the work.
+	 * Instantiate a CreateTable object and call.
+	 * For production, it returns the SQL script to create the tables.
+	 * We can change this for testing.
+	 */
+	public String createTables() {
+		defineTableSQL += "CREATE TABLE " + tableSchema + "." + tableName + " (" + primaryKeyColumnName
+				+ (primaryKeyIsSerial == true ? " SERIAL" : "") + " PRIMARY KEY, ";
+
+		defineTableSQL += getSQLType(columns);
+
+		for (int i = 0; i < foreignKeyColumns.size(); i++) {
+			defineTableSQL += getSQLType(foreignKeyColumns);
+		}
+
+		if (defineTableSQL.endsWith(", ")) {
+			defineTableSQL = defineTableSQL.substring(0, (defineTableSQL.length()) - 2);
+		}
+		
+		defineTableSQL += ");";
+
+		return defineTableSQL;
+
+	}
+
+	/** Utility method that determines type of columns and returns SQL based on the type.
+	 * Requires .getType() input - "String", "int", "double", "boolean" in the columns HashMap
+	 * for the key "columnType";
+	 * I'm think we can set this while doing meta models
+	 * If not, we can add it to the @Column and @JoinColumn annotations properties.
+	 */
+	public String getSQLType(List<LinkedHashMap<String, String>> cols) {
+
+		// Variable for SQL script to return
+		String sqlTypeReturn = "";
+		
+		if (cols != null) {
+
+			// Iterate through columns based on how many 
+			// works for @Columns and @JoinColumn columns
+			for (int i = 0; i < cols.size(); i++) {
+				String sqlType = "";
+				String columnName = cols.get(i).get("columnName").toString();
+				String columnType = cols.get(i).get("columnType").toString();
+				String isUnique = cols.get(i).get("isUnique").toString();
+				String isNullable = cols.get(i).get("isNullable").toString();
+				String columnLength = cols.get(i).get("columnLength").toString();
+				String columnPrecision = cols.get(i).get("columnPrecision").toString();
+	
+				if (columnType == "String") {
+					sqlType += "VARCHAR";
+					if (Integer.parseInt(columnLength) > 0) {
+						sqlType += "(" + columnLength + ")";
+					} else {
+						sqlType += "(50)";
+					}
+				} else if (columnType == "int") {
+					sqlType += "INTEGER";
+				} else if (columnType == "double") {
+					sqlType += "NUMERIC";
+					if (Integer.parseInt(columnLength) > 0 && Integer.parseInt(columnPrecision) > 0) {
+						sqlType += "(" + columnLength + "," + columnPrecision + ")";
+					} else if (Integer.parseInt(columnLength) > 0) {
+						sqlType += "(" + columnLength + ")";
+					} else {
+						sqlType += "(50)";
+					}
+				} else if (columnType == "boolean") {
+					sqlType = "BOOLEAN";
+				} else {
+					sqlType = null;
+				}
+	
+				sqlTypeReturn  += columnName + " " + sqlType + (isUnique.equals("true") ? " UNIQUE" : "")
+						+ (isNullable.equals("false") ? " NOT NULL" : "") + ", ";
+			}
+		}
+		return sqlTypeReturn;
+	}
 }
